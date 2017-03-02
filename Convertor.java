d012fedac156ed5e116b41e35885526839868df3
Snorri1986 <dionisiy1986@gmail.com>
2017-03-02 23:57:57 +0200

    1.Create new class WeatherTemp.java 2.Move methods showCelcius(int) and showFaringeith(int) to WeatherTemp.java  3.Start building Swing elements.

--- Convertor.java
+++ Convertor.java
@@ -1,9 +1,21 @@
-package tempconvertor.consol_version;
+/* Main class */
 import java.util.Scanner;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Convertor {
     public static void main(String[] args) {
-        int grade; // ввод пользовател€
-        char grade_system; // ввод пользовател€. ¬ какую систему пользователь желает перевести grade.
+        int grade;
+        char grade_system;
+        // JFrame preferences
+        JFrame frame = new JFrame("Converter");
+        frame.setVisible(true);
+        frame.setSize(500,400); // optimal size
+        // on the center of any screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+        WeatherTemp wt = new WeatherTemp();
         System.out.print("Please enter your grade value : ");
         Scanner sc1 = new Scanner(System.in);
         grade = sc1.nextInt();
@@ -11,28 +23,17 @@ public class Convertor {
         Scanner sc2 = new Scanner(System.in);
         grade_system = sc2.next().charAt(0);
         switch (grade_system) {
-            case 'c': System.out.println(getCelcius(grade,grade_system));
-                break;
-            case 'f': System.out.println(getFaringeith(grade,grade_system));
-                break;
+            case 'c':
+                      wt.showCelcius(grade);
+                      break;
+            case 'f': wt.showFaringeith(grade) ;
+                      break;
         }
     }
-    // метод подсчитывает значение в системе ÷ельси€ и возвращает значение в String
-    static String getCelcius(int g, char system) {
-        double result;
-        String answer = new String("Value in Celcius ");
-        if (system == 'c') result = (g-32)/1.8000;
-        else result = 0;
-        return answer + result + " C";
-    }
-    // метод подсчитывает значение в системе ‘арингейта и возвращает значение в String
-    static String getFaringeith(int g, char system) {
-        double result;
-        String answer = new String("Value in Faringeith ");
-        if (system == 'f') result = g*1.8000+32.00;
-        else result = 0;
-        return answer + result + " F";
-    }
+
+
+
+
 }
 
 
